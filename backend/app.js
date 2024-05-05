357b88de1ab7c9e25a3277a2e776cb905528ac0a
Labib Islam <84977406+Labib-i87@users.noreply.github.com>
2024-05-05 11:23:55 +0600

    db connect

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,17 @@
-console.log("hello world! jdskjfdkjfdskjnfdskjfndskjfdsn");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qv7f7hm.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
+  )
+  .then(() => {
+    app.listen(5000);
+    console.log("Connected to Database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
