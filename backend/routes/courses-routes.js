a39338f50c8bd8cc4681b89d082b3ad6711a6f5d
Labib Islam <84977406+Labib-i87@users.noreply.github.com>
2024-05-09 09:33:40 +0600

    Authentication Backend

--- backend/routes/courses-routes.js
+++ backend/routes/courses-routes.js
@@ -1,16 +1,19 @@
-const express = require('express');
-const { check } = require('express-validator');
+const express = require("express");
+const { check } = require("express-validator");
+const checkAuth = require("../middleware/check-auth");
 
 const router = express.Router();
 
-const coursesController = require('../controllers/courses-controller');
+const coursesController = require("../controllers/courses-controller");
 
-router.get('/', coursesController.getCourses);
-router.get('/:cid', coursesController.getCourseById);
-router.get('/sellercourses/:sid', coursesController.getCoursesBySellerId);
-router.get('/category/:category', coursesController.getCoursesByCategory);
+router.get("/", coursesController.getCourses);
+router.get("/:cid", coursesController.getCourseById);
+router.get("/sellercourses/:sid", coursesController.getCoursesBySellerId);
+router.get("/category/:category", coursesController.getCoursesByCategory);
+
+router.use(checkAuth("seller"));
 router.post("/new", coursesController.createCourse);
 router.patch("/:cid", coursesController.updateCourse);
-router.delete('/:cid', coursesController.deleteCourse);
+router.delete("/:cid", coursesController.deleteCourse);
 
 module.exports = router;
